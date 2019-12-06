31befa21f67a89c8bb48da41df0b3450c2ff492a
David Morton <55235981+davidmorton0@users.noreply.github.com>
2019-12-06 14:05:30 +0000

    add tests

--- first_non_repeating_character_v1.rb
+++ first_non_repeating_character_v1.rb
@@ -1,5 +1,13 @@
+require "test/unit/assertions"
+include Test::Unit::Assertions
+
 def  first_non_repeating_letter(s)
   result = s.chars.find { |c| (c == '?' ? s.scan(Regexp.new("#{c}")) :
                         s.scan(Regexp.new("#{c}", Regexp::IGNORECASE))).count == 1 }
   return result == nil ? '' : result
 end
+
+assert_equal(first_non_repeating_letter('a'), 'a')
+assert_equal(first_non_repeating_letter('stress'), 't')
+assert_equal(first_non_repeating_letter('moonmen'), 'e')
+assert_equal(first_non_repeating_letter(''), '')
--- first_non_repeating_character_v2.rb
+++ first_non_repeating_character_v2.rb
@@ -1,5 +1,13 @@
+require "test/unit/assertions"
+include Test::Unit::Assertions
+
 def  first_non_repeating_letter(s)
   s = s.tr("?", "")
   result = s.chars.select { |c| s.scan(Regexp.new("#{c}", Regexp::IGNORECASE)).count == 1 }.first
   return result == nil ? '' : result
 end
+
+assert_equal(first_non_repeating_letter('a'), 'a')
+assert_equal(first_non_repeating_letter('stress'), 't')
+assert_equal(first_non_repeating_letter('moonmen'), 'e')
+assert_equal(first_non_repeating_letter(''), '')
